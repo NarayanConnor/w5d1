c20e403970d84eada7cbeaa55e285ee757fe77e7
dustin <dustin.adler@yahoo.com>
2021-07-12 13:41:30 -0500

    XCellCor!

--- skeleton 2/lib/p02_hashing.rb
+++ skeleton 2/lib/p02_hashing.rb
@@ -4,11 +4,20 @@ end
 
 class Array
   def hash
+    self.map.with_index {|ele, i| (ele + i).hash}.sum
   end
 end
 
+#self.each_with_index.inject {|acc, ele, i| (acc ^ ele) }
+
+# compare the elements using xor,
+# could use acc ^ ele
+# [[1, 0],[2, 1],[3, 2]]
+
 class String
   def hash
+    alpha = ("A".."z").to_a
+    self.split("").map.with_index {|ch, i| (alpha.index(ch) + i).hash}.sum
   end
 end
 
@@ -16,6 +25,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.each {|k, v| sum += (k.hash + v.hash) }
+    sum
   end
 end
